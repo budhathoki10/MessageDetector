18c792f9f1f8fcbdb83b7a4f5220b3300df09f69
budhathoki10 <budhathokikushal170@gmail.com>
2026-03-28 16:37:15 +0545

    added rate limiter and user can send message for gemini 3 times per minutes

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express= require("express")
 const app= express();
 const apps = require("./Message");
+const rateLimit = require('express-rate-limit');
 require('dotenv').config()
 app.use(express.json())
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 5, 
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
 
+app.use(limiter);
 app.use("/api",apps)
 app.listen(5000,()=>{
     console.log("server connected sucessfully")
